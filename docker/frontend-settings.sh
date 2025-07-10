99fedd2c61f3ce115752e431a96e843a1e7d06e1
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2025-07-10 16:09:31 +0200

    fix: update file search pattern to include all JavaScript files for environment variable injection

--- docker/frontend-settings.sh
+++ docker/frontend-settings.sh
@@ -1,5 +1,5 @@
 echo "Inject environment variables in main*.js files..."
-for f in $(find /usr/share/nginx/html/ -maxdepth 1 -type f -name 'main*.js')
+for f in $(find /usr/share/nginx/html/ -maxdepth 1 -type f -name '*.js')
 do
     sed -i \
         -e "s|__API_URL__|$API_URL|g" \
